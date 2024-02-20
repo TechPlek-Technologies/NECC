3979c80cf2208028529fb9cd7fe62cfe994047a9
Heena-Kapoor <hkkapoor257@gmail.com>
2024-02-20 13:34:29 +0530

    Updating Home Page

--- frontend/src/pages/HomeOne.js
+++ frontend/src/pages/HomeOne.js
@@ -6,7 +6,7 @@ const BannerOne = React.lazy(() => import("../components/BannerOne"));
 const AboutOne = React.lazy(() => import("../components/AboutOne"));
 // const BlogOne = React.lazy(() => import("../components/BlogOne"));
 // const ContactOne = React.lazy(() => import("../components/ContactOne"));
-// const CounterOne = React.lazy(() => import("../components/CounterOne"));
+const CounterOne = React.lazy(() => import("../components/CounterOne"));
 const FeatureOne = React.lazy(() => import("../components/FeatureOne"));
 const FooterBottomOne = React.lazy(() =>
   import("../components/FooterBottomOne")
@@ -15,7 +15,7 @@ const FooterOne = React.lazy(() => import("../components/FooterOne"));
 // const NavbarOne = React.lazy(() => import("../components/NavbarOne"));
 const PartnerOne = React.lazy(() => import("../components/PartnerOne"));
 // const PortfolioOne = React.lazy(() => import("../components/PortfolioOne"));
-const ServiceOne = React.lazy(() => import("../components/ServiceOne"));
+// const ServiceOne = React.lazy(() => import("../components/ServiceOne"));
 // const TeamOne = React.lazy(() => import("../components/TeamOne"));
 const TestimonialOne = React.lazy(() => import("../components/TestimonialOne"));
 // const WhyChooseUsOne = React.lazy(() => import("../components/WhyChooseUsOne"));
@@ -43,14 +43,14 @@ const HomeOne = () => {
           <AboutOne />
 
           {/* Service One */}
-          <ServiceOne />
+          {/* <ServiceOne /> */}
 
           {/* Why Choose Us One */}
           {/* <WhyChooseUsOne /> */}
 
           {/* Counter One */}
 
-          {/* <CounterOne /> */}
+          <CounterOne />
 
           {/* Team One */}
           {/* <TeamOne /> */}
